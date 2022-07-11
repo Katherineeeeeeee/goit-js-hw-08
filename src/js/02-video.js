e4c07d02b0c40bf8b4f57d00d91afea2fcf11888
Katherineeeeeeee <katherine.dudar.7@gmail.com>
2022-07-11 19:39:06 +0300

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,21 @@
+import Player from '@vimeo/player';
+
+const throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo(iframe);
+const player = new Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
-});
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
+const onPlay = function(data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+    console.log(parsedData.seconds);
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const savedData = localStorage.getItem('videoplayer-current-time');
+const parsedData = JSON.parse(savedData);
+
+
+player. setCurrentTime(parsedData.seconds);
+
