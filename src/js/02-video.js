10d64ce972dd39836bcc6a6bdfc8728c7013e0cc
Katherineeeeeeee <katherine.dudar.7@gmail.com>
2022-07-11 17:29:39 +0300

    new

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,11 @@
 
-// const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
 
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
+player.on('play', function() {
+    console.log('played the video!');
+});
 
-// player.getVideoTitle().then(function(title) {
-//     console.log('title:', title);
-// });
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
