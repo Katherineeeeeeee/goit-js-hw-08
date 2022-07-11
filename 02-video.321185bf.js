5d18fec51092e108c6cfa79e429a5aa59e82b82a
Katherineeeeeeee <104833965+Katherineeeeeeee@users.noreply.github.com>
2022-07-11 16:40:09 +0000

    Deploying to gh-pages from @ Katherineeeeeeee/goit-js-hw-08@c7a252387b8181c8087e4ca077116caa21c49b57 🚀

--- 02-video.321185bf.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),o=new Vimeo(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)}))}();
-//# sourceMappingURL=02-video.321185bf.js.map
--- 02-video.974ecb63.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),o=new Vimeo(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)}));
-//# sourceMappingURL=02-video.974ecb63.js.map
